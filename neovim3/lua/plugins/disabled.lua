ab436fe6770ae299bd165f5132e97bf42a68db75
Stephen Lee <stephenl7797@gmail.com>
2025-07-02 09:06:46 -0400

    add neovim3

---
+++ neovim3/lua/plugins/disabled.lua
@@ -0,0 +1,36 @@
+return {
+  { "folke/noice.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = true },
+      dashboard = { enabled = true },
+      explorer = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = false },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+      scratch = {enabled=false},
+      terminal = {
+        win={style="float"},
+      }
+    },
+    keys = {
+    }
+  }
+
+}
